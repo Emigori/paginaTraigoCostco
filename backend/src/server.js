1f90e4372f70dfcb5d0f12e2782eb7da50b3167d
Emigori <emilianodelt@gmail.com>
2026-02-17 16:30:45 -0600

    Connection Express and MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,10 @@
 import "dotenv/config";
 import app from "./app.js";
+import { connectDB } from "./config/db.js";
 
 const PORT = process.env.PORT || 4000;
+
+await connectDB();
 
 app.listen(PORT, () => {
   console.log(`API running on http://localhost:${PORT}`);
